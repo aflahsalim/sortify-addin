7744eded49b2c4f3c78be45acf6004fc7dd1868d
Aflah <111690022+aflahsalim@users.noreply.github.com>
2026-03-21 23:50:11 +0100

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -192,7 +192,7 @@ function classifyEmail(emailText, hasAttachment, hasLinks, item) {
     senderReputation = "Suspicious";
   }
 
-  fetch("https://sortify-y7ru.onrender.com/classify", {
+  fetch("https://sortify-backend-hwf9d0exgqdub9cn.canadacentral-01.azurewebsites.net/predict", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
